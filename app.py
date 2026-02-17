a603a2b447c7a6096a25455acb3b411212584dae
spoorthireddy-17 <spoorthireddy006@gmail.com>
2026-02-17 15:27:56 +0530

    added model

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown("---")
 st.markdown("This application uses **Isolation Forest** to detect anomalies in air quality data.")
  
 # Load the trained model
-model_path= joblib.load("isolation_forest_air_quality.pkl")
+model_path= joblib.load("isolation_forest_model.pkl")
  
 try:
     model = joblib.load(model_path)
